8b0cd076b50f920f516224a508d7445229076791
JakubNemecekHub <jakubnemecek@hotmail.com>
2025-05-16 20:53:00 +0200

    Step 3
    Concatenate input files into standard output.

--- src/lib.rs
+++ src/lib.rs
@@ -5,37 +5,47 @@ use std::{
 };
 
 pub enum Config {
+    Man,
     Stdin,
-    File(String),
+    File(Vec<String>),
 }
 
 impl Config {
 
     pub fn build(mut args: impl Iterator<Item=String>) -> Result<Config, &'static str> {
         args.next();
-        match args.next() {
-            Some(value) => {
-                if value == "-" {
-                    return Ok(Config::Stdin);
-                } else {
-                    return Ok(Config::File(value));
-                }
-            },
-            None => return Err("Couldn't parse arguments."),
-        };
+        let all_args: Vec<String> = args.collect();
+        if all_args.is_empty() {
+            Ok(Config::Man)
+        } else if all_args[0] == "-" {
+            Ok(Config::Stdin)
+        } else {
+            Ok(Config::File(all_args))
+        }
     }
+
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     match config {
-        Config::Stdin => read_stdin(),
-        Config::File(file_path) => read_file(&file_path),
+        Config::Man => show_manual(),
+        Config::Stdin => read_stdin()?,
+        Config::File(file_path) => read_file(&file_path)?,
     }
+    Ok(())
 }
 
-pub fn read_file(file_path: &str) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(file_path)?;
-    println!("{}", contents);
+fn show_manual() -> () {
+    println!("Usage: cat [OPTION]... [FILE]...");
+    println!("Description: Concatenate FILE(s), or standard input, to standard output.");
+}
+
+pub fn read_file(file_paths: &Vec<String>) -> Result<(), Box<dyn Error>> {
+
+    for file_path in file_paths {
+        let contents = fs::read_to_string(file_path)?;
+        println!("{}", contents);
+    }
     Ok(())
 }
 
