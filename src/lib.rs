1099fb738d4bdca1e6c4840bfa3a202020a577c9
Jakub Němeček <jakubnemecek@hotmail.com>
2026-04-24 12:19:23 +0200

    Remove intermediate function
    The run function called the print_lines function with the result of
    either read_file_lines or read_stdin_lines as argument. Now we first
    call these read function and map their result (even thought it's one
    thing) and pass it to the emit function.

--- src/lib.rs
+++ src/lib.rs
@@ -34,17 +34,15 @@ impl fmt::Display for Suffix {
 
 pub fn run(files: &Vec<&str>, config: &Config) -> Result<(), Box<dyn Error>> {
     let mut counter: i32 = 0;
-    if files.is_empty() {
-        counter += print_lines(read_stdin_lines(), config, counter)?;
-    }
-    for &file in files {
-        if file != "-" {
-            match print_lines(read_file_lines(file), config, counter) {
+    let targets = if files.is_empty() { vec!["-"]} else { files.to_vec() };
+    for &target in &targets {
+        if target != "-" {
+            match read_file_lines(target).map(|lines| emit(lines, counter, config)) {
                 Ok(delta) => counter += delta,
-                Err(_) => eprintln!("cat: {}: no such file or directory", file),
+                Err(_) => eprintln!("cat: {}: no such file or directory", target),
             }
         } else {
-            counter += print_lines(read_stdin_lines(), config, counter)?;
+            counter += read_stdin_lines().map(|lines| emit(lines, counter, config))?;
         }
     }
     Ok(())
@@ -62,18 +60,6 @@ fn read_stdin_lines() -> io::Result<StaticStdinLines> {
     let stdin = Box::leak(Box::new(io::stdin())).lock();
     let reader = io::BufReader::new(stdin);
     Ok(reader.lines())
-}
-
-fn print_lines<I, E>(lines_result: Result<I, E>, config: &Config, counter: i32) -> Result<i32, E>
-where I: Iterator<Item = io::Result<String>>,
-      E: From<io::Error> {
-    match lines_result {
-        Err(err) => Err(err),
-        Ok(lines) => {
-            let delta = emit(lines, counter, config);
-            Ok(delta)
-        },
-    }
 }
 
 /// Print lines
